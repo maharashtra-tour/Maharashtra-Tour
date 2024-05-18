556689aac84fdc3c5bae95c9953aa5475c1ca040
Gaurav <gauravhpanchal2@gmail.com>
2024-05-18 17:04:14 +0530

    Seperate pages added for popular routes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,6 +11,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
     },
     {
+      url: "https://garvtravels.in/mumbai-to-shirdi-cab",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://garvtravels.in/mumbai-to-pune-cab",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://garvtravels.in/mumbai-to-lonavala-cab",
+      lastModified: new Date(),
+    },
+    {
       url: "https://garvtravels.in/services",
       lastModified: new Date(),
     },
