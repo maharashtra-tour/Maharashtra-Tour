485372110996118e0b9eb285a7164446ea1626b6
Gaurav <gauravhpanchal2@gmail.com>
2024-05-19 18:40:22 +0530

    sitemap changed

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://garvtravels.in/",
+      url: "https://www.garvtravels.in/",
       lastModified: new Date(),
     },
     {
-      url: "https://garvtravels.in/mumbai-darshan-cab",
+      url: "https://www.garvtravels.in/mumbai-darshan-cab",
       lastModified: new Date(),
     },
     {
-      url: "https://garvtravels.in/mumbai-to-shirdi-cab",
+      url: "https://www.garvtravels.in/mumbai-to-shirdi-cab",
       lastModified: new Date(),
     },
     {
-      url: "https://garvtravels.in/mumbai-to-pune-cab",
+      url: "https://www.garvtravels.in/mumbai-to-pune-cab",
       lastModified: new Date(),
     },
     {
-      url: "https://garvtravels.in/mumbai-to-lonavala-cab",
+      url: "https://www.garvtravels.in/mumbai-to-lonavala-cab",
       lastModified: new Date(),
     },
     {
-      url: "https://garvtravels.in/services",
+      url: "https://www.garvtravels.in/services",
       lastModified: new Date(),
     },
     {
-      url: "https://garvtravels.in/contact",
+      url: "https://www.garvtravels.in/contact",
       lastModified: new Date(),
     },
   ];
