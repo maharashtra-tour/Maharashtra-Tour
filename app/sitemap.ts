5459f8961bf230c40f01b96af6e02cdc84530f9e
Gaurav <gauravhpanchal2@gmail.com>
2024-07-02 14:48:38 +0530

    Redirect error fixing

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.maharashtratour.in/",
+      url: "https://maharashtratour.in/",
       lastModified: new Date(),
     },
     {
-      url: "https://www.maharashtratour.in/mumbai-darshan-cab",
+      url: "https://maharashtratour.in/mumbai-darshan-cab",
       lastModified: new Date(),
     },
     {
-      url: "https://www.maharashtratour.in/mumbai-to-shirdi-cab",
+      url: "https://maharashtratour.in/mumbai-to-shirdi-cab",
       lastModified: new Date(),
     },
     {
-      url: "https://www.maharashtratour.in/mumbai-to-pune-cab",
+      url: "https://maharashtratour.in/mumbai-to-pune-cab",
       lastModified: new Date(),
     },
     {
-      url: "https://www.maharashtratour.in/mumbai-to-lonavala-cab",
+      url: "https://maharashtratour.in/mumbai-to-lonavala-cab",
       lastModified: new Date(),
     },
     {
-      url: "https://www.maharashtratour.in/services",
+      url: "https://maharashtratour.in/services",
       lastModified: new Date(),
     },
     {
-      url: "https://www.maharashtratour.in/contact",
+      url: "https://maharashtratour.in/contact",
       lastModified: new Date(),
     },
   ];
